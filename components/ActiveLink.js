3435754dcfcd76209ef75da50b180206cd8e9291
Keaton Robert Gallagher <keaton@keaton.local>
2019-11-18 13:43:36 -0700

    Created the ablity to have active links, and added prefetching

--- components/ActiveLink.js
+++ components/ActiveLink.js
@@ -1,5 +1,38 @@
-const ActiveLink = () => {
-  return <div>ActiveLink</div>;
+import { withRouter } from 'next/router'
+import green from "@material-ui/core/colors/green";
+
+const ActiveLink = ({ router, href, children }) => {
+
+  (function preFetchPages() {
+    if(typeof window !== undefined) {
+      //Best works in production
+      router.prefetch(router.pathname)
+    }
+
+  })() //Creating it as an ify calling the function right away by wrapping it int parentheses
+
+
+  const handleClick = event => {
+    event.preventDefault();
+    router.push(href);
+  }
+  const isCurrentPath = router.pathname === href || router.asPath === href;
+
+  return<div>
+    <a 
+      href={href} 
+      onClick={handleClick} 
+      style={
+        {
+          textDecoration:'none', 
+          margin:'0', 
+          padding:'0', 
+          fontWeight: isCurrentPath ? "bold" : "normal",
+          color: isCurrentPath ? "black" : '#fff'
+        }}>
+      {children}
+    </a>
+  </div>;
 };
 
-export default ActiveLink;
+export default withRouter(ActiveLink);
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,56 +1,69 @@
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import ShareOutlined from "@material-ui/icons/ShareOutlined";
-import withStyles from "@material-ui/core/styles/withStyles";
-import color from "@material-ui/core/colors/yellow";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import ShareOutlined from '@material-ui/icons/ShareOutlined';
+import withStyles from '@material-ui/core/styles/withStyles';
+import activeLink from './ActiveLink';
+import ActiveLink from './ActiveLink';
 
-const Navbar = ({ classes, router, pageProps:{ auth } }) => {
-  //Grab user
-  const { user = {} } = auth || {};
+const Navbar = ({ classes, router, pageProps: { auth } }) => {
+	//Grab user
+	const { user = {} } = auth || {};
 
-  return (
-    <AppBar className={classes.appBar} position={router.pathname === '/' ? 'fixed' : 'static'}>
-      <Toolbar>
-        <ShareOutlined className={classes.icon} />
-        <Typography variant="h5" component="h1" className={classes.toolbarTitle}>
-            Next Chat
-        </Typography>
-        {user._id ? (
-          // Auth Navigation
-          <div>
-            <Button className={classes.button}>Profile</Button> 
-            <Button className={classes.button} variant="outlined">Sign Out</Button>
-          </div>
-        ): (
-          // unAuth Navigation
-          <div>
-            <Button className={classes.button}>
-              Sign In
-            </Button>
-            <Button className={classes.button}>
-              Sign Up
-            </Button>
-          </div>
-        )}
-      </Toolbar> 
-    </AppBar>
-  )
+	return (
+		<AppBar className={classes.appBar} position={router.pathname === '/' ? 'fixed' : 'static'}>
+			<Toolbar>
+				<ActiveLink href="/">
+					<ShareOutlined className={classes.icon} />
+				</ActiveLink>
+				<Typography variant="h5" component="h1" className={classes.toolbarTitle}>
+					<ActiveLink href="/">Next Chat</ActiveLink>
+				</Typography>
+				{user._id ? (
+					// Auth Navigation
+					<div>
+						<Button className={classes.button}>
+							<ActiveLink href="/profile">Profile</ActiveLink>
+						</Button>
+						<Button className={classes.button} variant="outlined">
+							Sign Out
+						</Button>
+					</div>
+				) : (
+					// unAuth Navigation
+					<div>
+						<Button className={classes.button}>
+							<ActiveLink href="/signin">Sign In</ActiveLink>
+						</Button>
+						<Button className={classes.button}>
+							<ActiveLink href="/signup">Sign Up</ActiveLink> 
+						</Button>
+					</div>
+				)}
+			</Toolbar>
+		</AppBar>
+	);
 };
 
-const styles = theme => ({
-  appBar: {
-    // z-index 1 higher than the fixed drawer in home page to clip it under the navigation
-    zIndex: theme.zIndex.drawer + 1,
-    color:  "#fff"
-  },
-  toolbarTitle: {
-    flex: 1,    
-  },
-  icon: {
-    marginRight: theme.spacing.unit
-  }
+const styles = (theme) => ({
+	appBar: {
+		// z-index 1 higher than the fixed drawer in home page to clip it under the navigation
+		zIndex: theme.zIndex.drawer + 1,
+		color: '#fff'
+	},
+	toolbarTitle: {
+		flex: 1,
+		color: '#fff',
+		fontWeight: 600
+	},
+	icon: {
+		marginRight: theme.spacing.unit
+	},
+	button: {
+		color: '#fff',
+		fontWeight: 600
+	}
 });
 
 export default withStyles(styles)(Navbar);
